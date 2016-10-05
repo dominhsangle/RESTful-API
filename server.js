beb597692d5ec02bd10e0cc824881f97e57b5bc4
Sang Le <dominhsang.le@gmail.com>
2016-10-05 08:53:13 +0000

    add middleware

--- server.js
+++ server.js
@@ -7,6 +7,10 @@
 var express    = require("express");
 var app        = express();
 var bodyParser = require("body-parser");
+var mongoose   = require("mongoose");
+var Bear       = require("./app/models/bear");
+
+mongoose.connect("mongodb://localhost/restful-api");
 
 // config app to use bodyParser()
 // this will let us get the data from a POST
@@ -18,6 +22,12 @@ var port = process.env.PORT || 8080; // set our port
 // ROUTES FOR OUR API 
 // =============================================================================
 var router = express.Router();
+
+// middleware to use for all requests
+router.use(function(req, res, next){
+    console.log("Something is happen");
+    next();
+});
 
 // test route to make sure everything is working
 router.get("/", function(req, res){
